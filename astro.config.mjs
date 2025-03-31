66123ca80c8c9cdcf2eb10088e5d38854dbca399
nachomf18 <118490509+nachomf18@users.noreply.github.com>
2025-03-31 19:22:54 +0200

    Styling Header, Hero, Contact and About

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  experimental: { 
+    svg: {
+      mode: 'sprite',
+    }
   }
 });
